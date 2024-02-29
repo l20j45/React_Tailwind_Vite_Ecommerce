4bb171b265831c1f780a19b533f220ed4be4c90e
Daniel Rojas <l20j45_@gmail.com>
2024-02-28 21:05:48 -0600

    Cree el servicio, mande a llamar el servicio desde un archivo exterño, cree el enviroment, y asi comenzamos a usar todo lo que he aprendido

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,18 +1,10 @@
-import {useEffect, useState} from "react";
+
 import Card from "../../Components/Card/index.jsx";
+import {GetProducts} from "../../services/productsService.jsx";
 
 function Home() {
-    const [items, setItems] = useState(null)
-    useEffect(() => {
-        fetch('https://api.escuelajs.co/api/v1/products')
-            .then(res => res.json())
-            .then(data => setItems(data))
-            .catch(err => {
-                console.log('caught it!', err);
-            });
 
-    }, [])
-
+    const items = GetProducts();
     return (
 
         <div className='grid gap-4 grid-cols-4 w-full max-w-screen-lg'>
