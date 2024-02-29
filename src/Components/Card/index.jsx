a2575ff1b03280193ccb17933cd5e68e7a5826da
Daniel Rojas <l20j45_@gmail.com>
2024-02-29 10:46:20 -0600

    Solo destructure el provider, no veo necesario hacer mas cambios

--- src/Components/Card/index.jsx
+++ src/Components/Card/index.jsx
@@ -2,7 +2,7 @@ import {useContext} from "react";
 import {ShoppingCartContext} from "../../Context/index.jsx";
 
 const Card = ({data}) => {
-    const context = useContext(ShoppingCartContext)
+    const {count,setCount} = useContext(ShoppingCartContext)
 
     return (
         <div className='bg-white cursor-pointer w-56 h-60 rounded-lg'>
@@ -10,11 +10,11 @@ const Card = ({data}) => {
                 <span
                     className='absolute bottom-0 left-0 bg-white/60 rounded-lg text-black text-xs m-2 px-3 py-0.5'>{data.category.name}</span>
                 <img className='w-full h-full object-cover rounded-lg' src={data.images[0]} alt={data.title}/>
-                <div
+                <button
                     className='absolute top-0 right-0 flex justify-center items-center bg-white w-6 h-6 rounded-full m-2 p-1'
-                    onClick={() => context.setCount(context.count + 1)}>
+                    onClick={() => setCount(count + 1)}>
                     +
-                </div>
+                </button>
             </figure>
             <p className='flex justify-between'>
                 <span className='text-sm font-light'>{data.title}</span>
